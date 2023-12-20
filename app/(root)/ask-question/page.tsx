1cf010ee7949d801f31e03a4312c71ab9c36b7c4
720 <kaplann.oskar@gmail.com>
2023-12-20 17:46:57 +0100

    add auth

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,10 @@
+const Page = () => {
+  return (
+    <main>
+      <h2>Ask a question</h2>
+      <input className="border" type="text" placeholder="eg. How to code?" />
+    </main>
+  )
+}
+
+export default Page
