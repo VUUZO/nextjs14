74a8a22d02d3f6ce29f4d1175c2e5593637452cb
720 <kaplann.oskar@gmail.com>
2023-12-29 00:14:05 +0100

    Inclode form

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,9 +1,13 @@
+import { Question } from "@/components/forms/Question"
+
 const Page = () => {
   return (
-    <main>
-      <h2>Ask a question</h2>
-      <input className="border" type="text" placeholder="eg. How to code?" />
-    </main>
+    <div>
+      <h1 className="h1-bold text-dark100_light900">Ask a Question</h1>
+      <div className="mt-9">
+        <Question />
+      </div>
+    </div>
   )
 }
 
