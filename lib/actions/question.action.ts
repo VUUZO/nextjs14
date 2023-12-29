3af55fabbbc5f61d067f4e9b7f71878ab586bff8
720 <kaplann.oskar@gmail.com>
2023-12-29 22:26:44 +0100

    Add server actions files

---
+++ lib/actions/question.action.ts
@@ -0,0 +1,59 @@
+'use server'
+
+import Question from "@/database/question.model"
+import { connectToDatabase } from "../db"
+import { CreateQuestionParams, GetQuesitonsParams } from "./shared.types"
+import Tag from "@/database/tag.model"
+import User from "@/database/user.model"
+import { revalidatePath } from "next/cache"
+
+export const getQuestions = async (params: GetQuesitonsParams) => {
+  try {
+    connectToDatabase()
+    const questions = await Question.find({})
+      .populate({ path: 'tags', model:  Tag })
+      .populate({ path: 'author', model: User })
+      .sort({ createdAt: -1 })
+
+    return { questions }
+
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export async function createQuestion (params: CreateQuestionParams) {
+  try {
+    connectToDatabase()
+
+    const { title, content, tags, author, path } = params
+
+    const question = await Question.create({
+      title,
+      content,
+      author
+    })
+
+    const tagDocuments = []
+
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}`, "i") } },
+        { $setOnInsert: { name: tag }, $push: { question: question._id } },
+        { upsert: true, new: true },
+      )
+
+      tagDocuments.push(existingTag._id)
+    }
+
+    await Question.findByIdAndUpdate(question._id, {
+      $push: { tags: { $each: tagDocuments } }
+    })
+
+    revalidatePath(path)
+
+  } catch (error) {
+    console.log(error)
+  }
+}
